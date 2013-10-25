946cc1b25dc9209fc05f59bb55cb225b3240dd38
Decio Battaglia <decio.battaglia@gmail.com>
2013-10-25 13:03:26 +0100

    added percentage operation to script

--- get_raster_stats.rb
+++ get_raster_stats.rb
@@ -11,7 +11,7 @@ iso2_codes = JSON.parse(json, :symbolize_names => true)
 def parse(args)
   options = OpenStruct.new
   options.url_root = "http://raster-stats.unep-wcmc.org/"
-  options.raster_id = 3 # carbon
+  options.raster_ids = [1] # carbon
   options.operation = "count"
   options.country = nil
   
@@ -28,8 +28,10 @@ def parse(args)
         options.url_root = url_root
       end
     end
-    opts.on("-i", "--id [INTEGER]", Integer, "Raster id") do |id|
-      options.id = id
+    opts.on("-i", "--ids [INTEGER]", Array, 
+            "Comma separated list of Raster ids",
+            "  If more than one, operation must be percentage") do |raster_ids|
+      options.raster_ids = raster_ids
     end
     opts.on("-o", "--operation [STRING]", String, 
             "Name of the statistical operation, choose from:",
@@ -49,49 +51,74 @@ end
 options = parse(ARGV)
 
 
-def execute_query country, options
-  uri = URI("#{ options.url_root }rasters/#{ options.id }/" + 
+def execute_query options
+  uri = URI("#{ options.url_root }rasters/#{ options.raster_id }/" + 
             "operations/#{ options.operation }")
-  params = { :iso2 => country }
+  params = { :iso2 => options.country }
   uri.query = URI.encode_www_form(params)
   begin
     res = Net::HTTP.get_response(uri)
   rescue  Exception => e  
-    pp country, e.message
+    pp options.country, e.message
   end
   #puts res.body if res.is_a?(Net::HTTPSuccess)
   begin
     JSON.parse(res.body, :symbolize_names => true)
   rescue
-    pp "Error for #{country}"
+    pp "Error for #{options.country}"
     nil
   end
 end
 
+def get_percentage_object_list options
+  percentage_object_list = []
+  opt = options.clone
+  options.raster_ids.each do |raster_id|
+    opt.raster_id = Integer(raster_id)
+    res = execute_query opt
+    if res
+      res[:iso2] = opt.country
+      res[:raster_id] = raster_id
+      percentage_object_list << res
+    end
+  end
+  percentage_object_list
+  tot = 0
+  percentage_object_list.each{|obj| tot += obj[:value]}
+  percentage_object_list.each{|obj| 
+    obj[:percentage] = obj[:value] / tot * 100
+  }
+  percentage_object_list
+end
+
+opt = options.clone
 all_countries_accumulator = []
 
-unless options.operation == "percentage"
-  country = options.country
-  if country
-    execute_query country, options
+unless opt.operation == "percentage"
+  opt.raster_id = options.raster_ids[0]
+  if opt.country
+    pp execute_query opt
   else
     iso2_codes.each do |feature|
-      country = feature[:alpha_2]
-      res = execute_query country, options   
+      opt.country = feature[:alpha_2]
+      res = execute_query opt
       if res
-        res[:iso2] = country
+        res[:iso2] = opt.country
         all_countries_accumulator << res
-        pp res
       end
     end
     pp all_countries_accumulator
   end
 else
-  if country
-    #options.operation = ""
-    #res_one = execute_query country, options
-    pp "percentage to be implemented"
+  # Relying on a few assumptions here:
+  # a percentage calculation expects a list of rasters...
+  options.operation = "sum"
+  if options.country
+    pp get_percentage_object_list options
   else
-    pp "percentage for all countries to be implemented"
+    iso2_codes.each do |feature|
+      all_countries_accumulator << get_percentage_object_list(options)
+    end
+    pp all_countries_accumulator
   end
 end
